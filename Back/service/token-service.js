c587590814c94d9a0a5d82cc3d9d5644a9e83c0d
Empty13-13 <maks.ept1@gmail.com>
2024-03-11 19:34:02 +0500

    Start Registration

--- Back/service/token-service.js
+++ Back/service/token-service.js
@@ -1,24 +1,32 @@
 const jwt = require('jsonwebtoken')
 const tokenModel = require('../model/models')
-class TokenService{
-    generateTokens(payload){
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET,{expiresIn: '15m'})
-        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET,{expiresIn: '30d'})
-        return{
-            accessToken,
-            refreshToken
-        }
 
+class TokenService {
+  generateTokens(payload) {
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '15m'})
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30d'})
+    return {
+      accessToken,
+      refreshToken
     }
-    async saveToken(userId,refreshToken){
-        const tokenData = await  tokenModel.Token.findOne({where:{userId}})
-        if (tokenData){
-            console.log('error')
-            tokenData.refreshToken = refreshToken
-            return tokenData.save()
-        }
-        const token = await  tokenModel.Token.create({userId,refreshToken})
-        return token;
+    
+  }
+  
+  async saveToken(userId, refreshToken) {
+    const tokenData = await tokenModel.Token.findOne({where: {userId}})
+    if (tokenData) {
+      console.log('error')
+      tokenData.refreshToken = refreshToken
+      return tokenData.save()
     }
+    const token = await tokenModel.Token.create({userId, refreshToken})
+    return token;
+  }
+  
+  async removeToken(refreshToken) {
+    const tokenData = await tokenModel.Token.destroy({where: {refreshToken: refreshToken}})
+    return tokenData
+  }
 }
+
 module.exports = new TokenService()
