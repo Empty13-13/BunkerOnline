671cf82409a3fbace089c94b912a0ca695c18da4
Empty13-13 <maks.ept1@gmail.com>
2024-05-29 17:32:59 +0500

    Fix Authorization in isUserBlocked-middleware.js

--- Back/middlewares/isUserBlocked-middleware.js
+++ Back/middlewares/isUserBlocked-middleware.js
@@ -9,21 +9,23 @@ module.exports = async function(req, res, next) {
       let accessToken = null
       if (authorizationHeader && authorizationHeader.toString().includes('Bearer ')) {
         accessToken = authorizationHeader.split('Bearer ')[1]
+
+        if (!accessToken) {
+          return next(ApiError.UnauthorizedError())
+        }
+        const userData = tokenService.validateAccessToken(accessToken)
+        console.log(userData)
+        if (!userData) {
+          return next(ApiError.UnauthorizedError())
+        }
+        const isBlocked = await UserModel.BlackListUsers.findOne({where: {userId: userData.id}})
+        if (isBlocked) {
+          return next(ApiError.BlockedUser())
+        }
       }
 
-      if (!accessToken) {
-        return next(ApiError.UnauthorizedError())
-      }
-      const userData = tokenService.validateAccessToken(accessToken)
-      console.log(userData)
-      if (!userData) {
-        return next(ApiError.UnauthorizedError())
-      }
-       const isBlocked = await UserModel.BlackListUsers.findOne({where: {userId: userData.id}})
-       if(isBlocked){
-         return next(ApiError.BlockedUser())
-       }
-       // next()
+
+      // next()
     }
     next()
   } catch(e) {
