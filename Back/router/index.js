c7e25d0bd8e09339671b1dae67b29dd62da8daf4
Empty13-13 <maks.ept1@gmail.com>
2024-07-12 19:49:53 +0500

    Fix NoRegToken

--- Back/router/index.js
+++ Back/router/index.js
@@ -131,7 +131,7 @@ router.get('/otherText/:id', userController.loadOtherText);
 router.get('/wikiList', userController.loadWikiList);
 router.post('/generateKeys', adminMiddleware('admin'), adminController.generateKeys);
 router.post('/activateKey', authMiddleware, userController.activateKey);
-router.get('/pricesInfo', authMiddleware, userController.getPriceInfo);
+router.get('/pricesInfo', userController.getPriceInfo);
 router.get('/getHomeImageName', userController.getHomeImageName);
 router.get('/getBaseProfession', userController.getBaseProfession);
 router.get('/getUpdateInfo', userController.getUpdateInfo);
